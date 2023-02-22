b9d243e56feaa878aeabef753bc0964a65c8ecb7
Matthew Lim <matlim200@gmail.com>
2023-02-22 16:04:53 -0700

    add each method to solve problems

--- each.rb
+++ each.rb
@@ -11,3 +11,13 @@ weather_options = ["sunny", "snowy", "freezing", "partly cloudy", "drizzly"]
 words = ["car", "bike", "bus", "van", "scooter", "truck", "rv"]
 
 # Record yourself live-coding and talking through either Challenge 1 or 2. The recording doesn't have to be the first time you are solving the problem. Use technical vocabulary and explain each part to demonstrate your understanding! Share the link to your video in your small group channel.
+
+weather_options.each do |option|
+  puts "The weather today will be #{option}."
+end
+
+words.each do |word|
+if word.length == 3
+  puts word
+end
+end
